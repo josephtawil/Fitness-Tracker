7f8bdfe35e6aa4f8e627984d99bb5409b2f767a1
Joseph <jtawil2015@gmail.com>
2020-08-02 16:06:45 -0800

    saved routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {getWorkout} = require("../seeders/seed");
 
-
+//getting workouts
 router.get('/api/workouts', getWorkout);
 
 module.exports = router;
